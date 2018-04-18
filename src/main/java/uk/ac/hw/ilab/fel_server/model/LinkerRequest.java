14daf833899bb8fca14705b0721b2ccee2f40edd
Alessandro Suglia <alessandro.suglia@gmail.com>
2018-04-18 23:58:40 +0100

    Annotations process
    1. Introduced profanity filtering
    2. Optimised linking procedure using annotation and candidates thresholds

--- src/main/java/uk/ac/hw/ilab/fel_server/model/LinkerRequest.java
+++ src/main/java/uk/ac/hw/ilab/fel_server/model/LinkerRequest.java
@@ -6,6 +6,7 @@ public class LinkerRequest {
     private String text;
     private Multimap<String, String> properties;
     private Multimap<String, String> context;
+    private Multimap<String, String> profanity;
 
     public String getText() {
         return text;
@@ -31,12 +32,21 @@ public class LinkerRequest {
         this.properties = properties;
     }
 
+    public Multimap<String, String> getProfanity() {
+        return profanity;
+    }
+
+    public void setProfanity(Multimap<String, String> profanity) {
+        this.profanity = profanity;
+    }
+
     @Override
     public String toString() {
         return "LinkerRequest{" +
                 "text='" + text + '\'' +
                 ", properties= " + properties +
                 ", context=" + context +
+                ", profanity=" + profanity +
                 '}';
     }
 }
